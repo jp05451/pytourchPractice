a4c49ccd4937eaa62b6cb8aadb6d1c823a6952e5
issues <jp05451@yahoo.com.tw>
2024-01-17 13:28:37 +0800

    add dataLoader

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset,DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
@@ -18,19 +18,21 @@ test_data = datasets.FashionMNIST(
     download=True,
     transform=ToTensor()
 )
+    
 
 labels_map = {
-    0: "T-Shirt",
-    1: "Trouser",
-    2: "Pullover",
-    3: "Dress",
-    4: "Coat",
-    5: "Sandal",
-    6: "Shirt",
-    7: "Sneaker",
-    8: "Bag",
-    9: "Ankle Boot",
+0: "T-Shirt",
+1: "Trouser",
+2: "Pullover",
+3: "Dress",
+4: "Coat",
+5: "Sandal",
+6: "Shirt",
+7: "Sneaker",
+8: "Bag",
+9: "Ankle Boot",
 }
+
 figure = plt.figure(figsize=(8, 8))
 cols, rows = 3, 3
 for i in range(1, cols * rows + 1):
@@ -41,3 +43,9 @@ for i in range(1, cols * rows + 1):
     plt.axis("off")
     plt.imshow(img.squeeze(), cmap="gray")
 plt.show()
+    
+
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+
